ecb00f4ed961a631fcc25042434f4c7163c411b7
Asmita Dutta <asdutta@ucsc.edu>
2017-01-15 21:02:17 -0800

    Add sample templates

--- project.py
+++ project.py
@@ -1,12 +1,27 @@
 import sys
-from flask import Flask
+from flask import (Flask, render_template, request, redirect,
+                   make_response, url_for, flash, jsonify)
+from flask import session as login_session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
+from database_setup import Base, Category, Item, User
+
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///catalogapp.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
-@app.route('/catalogs/')
-def catalogs():
-	return sys._getframe().f_code.co_name
+@app.route('/category/')
+def category():
+	return render_template('category_list.html',
+                           categories=session.query(Category).all(),
+                           items=session.query(Item).order_by('id DESC').limit(10).all(),
+                           )
 
 @app.route('/category/<int:category_id>/')
 def showItemsForCategory(category_id):
@@ -14,7 +29,10 @@ def showItemsForCategory(category_id):
 
 @app.route('/category/<int:category_id>/item/<int:item_id>/')
 def showItem(category_id, item_id):
-	return sys._getframe().f_code.co_name
+	return render_template('item.html',
+                           categories=session.query(Category).all(),
+                           item=session.query(Item, Category).filter_by(id=item_id).join(Category).one(),
+                           main_category=category_id)
 
 @app.route('/category/<int:category_id>/addItem', methods=['GET', 'POST'])
 def addItem(category_id):
