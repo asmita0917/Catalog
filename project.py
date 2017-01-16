26c8a1a1dc74df107059f9b09a5738a246d7ed0a
Asmita Dutta <asdutta@ucsc.edu>
2017-01-16 12:36:29 -0800

    GET api for add/edit/delete

--- project.py
+++ project.py
@@ -16,27 +16,32 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-def categories():
+# TODO: As try/catch
+def db_categories():
     return session.query(Category).order_by('name')
 
-def category(category_id):
+def db_category(category_id):
     return session.query(Category).filter_by(id=category_id).one()
 
-def item(item_id):
-        return session.query(Item, Category).filter_by(id=item_id).join(Category).one()
+def db_item(item_id):
+	try:
+		result = session.query(Item, Category).filter_by(id=item_id).join(Category).one()
+		return result[0]
+	except Exception as e:
+		return None
 
-def itemsLatestFirst():
+def db_itemsLatestFirst():
         return session.query(Item).order_by('id DESC').limit(10).all()
 
-def items(category_id):
+def db_items(category_id):
     return session.query(Item).filter_by(category_id=category_id).all()
 
-def addToDatabase(item):
+def db_addToDatabase(item):
     session.add(item)
     session.commit()
     return
 
-def deleteFromDatabase(item):
+def db_deleteFromDatabase(item):
     session.delete(item)
     session.commit()
     return
@@ -45,32 +50,51 @@ def deleteFromDatabase(item):
 @app.route('/category/')
 def category():
 	return render_template('category_list.html',
-                           categories=categories(),
-                           items=itemsLatestFirst(),
+                           categories=db_categories(),
+                           items=db_itemsLatestFirst(),
                            )
 
 @app.route('/category/<int:category_id>/')
-def showItemsForCategory(category_id):
-	return sys._getframe().f_code.co_name
+def showItems(category_id):
+	return render_template('category.html',
+                           main_category=db_category(category_id),
+                           categories=db_categories(),
+                           items=db_items(category_id))
 
 @app.route('/category/<int:category_id>/item/<int:item_id>/')
 def showItem(category_id, item_id):
 	return render_template('item.html',
-                           categories=categories(),
-                           item=item(item_id),
+                           categories=db_categories(),
+                           item=db_item(item_id),
                            main_category=category_id)
 
 @app.route('/category/<int:category_id>/addItem', methods=['GET', 'POST'])
 def addItem(category_id):
-	return sys._getframe().f_code.co_name
+	if request.method == 'POST':
+		return sys._getframe().f_code.co_name
+	else:
+		return render_template('addItem.html',
+        	category=db_category(category_id))
+
 
 @app.route('/category/<int:category_id>/editItem/<int:item_id>/', methods=['GET', 'POST'])
 def editItem(category_id, item_id):
-	return sys._getframe().f_code.co_name
+	if request.method == 'POST':
+		return sys._getframe().f_code.co_name
+	else:
+		return render_template('editItem.html',
+        	category=db_category(category_id),
+        	item=db_item(item_id))
 
 @app.route('/category/<int:category_id>/deleteItem/<int:item_id>/', methods=['GET', 'POST'])
 def deleteItem(category_id, item_id):
-	return sys._getframe().f_code.co_name
+	if request.method == 'POST':
+		return sys._getframe().f_code.co_name
+	else:
+		return render_template('deleteItem.html',
+        	category=db_category(category_id),
+        	item=db_item(item_id))
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
