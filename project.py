7943579a27994c01c7db0ed7856652a648787a89
Asmita Dutta <asdutta@ucsc.edu>
2017-01-16 16:14:49 -0800

    Add POST for add/edit/delete

--- project.py
+++ project.py
@@ -24,11 +24,11 @@ def db_category(category_id):
     return session.query(Category).filter_by(id=category_id).one()
 
 def db_item(item_id):
-	try:
-		result = session.query(Item, Category).filter_by(id=item_id).join(Category).one()
-		return result[0]
-	except Exception as e:
-		return None
+    try:
+        result = session.query(Item, Category).filter_by(id=item_id).join(Category).one()
+        return result[0]
+    except Exception as e:
+        return None
 
 def db_itemsLatestFirst():
         return session.query(Item).order_by('id DESC').limit(10).all()
@@ -49,61 +49,72 @@ def db_deleteFromDatabase(item):
 @app.route('/')
 @app.route('/category/')
 def category():
-	return render_template('category_list.html',
+    return render_template('category_list.html',
                            categories=db_categories(),
                            items=db_itemsLatestFirst(),
                            )
 
 @app.route('/category/<int:category_id>/')
 def showItems(category_id):
-	return render_template('category.html',
+    return render_template('category.html',
                            main_category=db_category(category_id),
                            categories=db_categories(),
                            items=db_items(category_id))
 
 @app.route('/category/<int:category_id>/item/<int:item_id>/')
 def showItem(category_id, item_id):
-	return render_template('item.html',
+    return render_template('item.html',
                            categories=db_categories(),
                            item=db_item(item_id),
                            main_category=category_id)
 
 @app.route('/category/<int:category_id>/addItem', methods=['GET', 'POST'])
 def addItem(category_id):
-	if request.method == 'POST':
-		return sys._getframe().f_code.co_name
-	else:
-		return render_template('addItem.html',
-        	category=db_category(category_id))
+    if request.method == 'POST':
+        item = Item(name=request.form['name'],
+                        description=request.form['description'],
+                        category_id=category_id)
+        db_addToDatabase(item)
+        return redirect(url_for('showItems', category_id=category_id))
+    else:
+        return render_template('addItem.html',
+            category=db_category(category_id))
 
 
 @app.route('/category/<int:category_id>/editItem/<int:item_id>/', methods=['GET', 'POST'])
 def editItem(category_id, item_id):
-	if request.method == 'POST':
-		return sys._getframe().f_code.co_name
-	else:
-		return render_template('editItem.html',
-        	category=db_category(category_id),
-        	item=db_item(item_id))
+    if request.method == 'POST':
+        item = db_item(item_id)
+        item.name=request.form['name']
+        item.description = request.form['description']
+        item.category_id =category_id
+        db_addToDatabase(item)
+        return redirect(url_for('showItems', category_id=category_id))
+    else:
+        return render_template('editItem.html',
+            category=db_category(category_id),
+            item=db_item(item_id))
 
 @app.route('/category/<int:category_id>/deleteItem/<int:item_id>/', methods=['GET', 'POST'])
 def deleteItem(category_id, item_id):
-	if request.method == 'POST':
-		return sys._getframe().f_code.co_name
-	else:
-		return render_template('deleteItem.html',
-        	category=db_category(category_id),
-        	item=db_item(item_id))
+    if request.method == 'POST':
+        item = db_item(item_id)
+        db_deleteFromDatabase(item)
+        return redirect(url_for('showItems', category_id=category_id))
+    else:
+        return render_template('deleteItem.html',
+            category=db_category(category_id),
+            item=db_item(item_id))
 
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-	return sys._getframe().f_code.co_name
+    return sys._getframe().f_code.co_name
 
 
 @app.route('/gdisconnect')
 def gdisconnect():
-	return sys._getframe().f_code.co_name
+    return sys._getframe().f_code.co_name
 
 
 if __name__ == '__main__':
